209d3548fadba967db2f52340afe2ecd0590fafb
Hellpid <biba798999@gmail.com>
2025-01-18 11:14:48 +0300

    Update main.py
    dobavil ugol pi/4

--- main.py
+++ main.py
@@ -1,12 +1,13 @@
 import matplotlib.pyplot as plt
 import numpy as np
+from math import pi
 from classes.square import Square
 from classes.point import Point
 from functions.plot_streamlines import plot_streamlines
 from functions.plot_trajectory import plot_trajectory
 
 
-a = Square(1, Point(-1.5, -1.5))
+a = Square(1, Point(-1.5, -1.5), angle=pi/4)
 plt.plot(list(map(lambda p: p.x, a.points)), list(map(lambda p: p.y, a.points)))
 plt.grid(color='gray', linestyle='--', linewidth=0.5)
 plt.show()
